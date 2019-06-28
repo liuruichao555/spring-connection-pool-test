0b9c6b08bdd34311492089deb4d48ce9d5138c4d
liuruichao <liuruichao555@126.com>
2019-06-28 13:30:55 +0800

    update

--- src/main/java/com/liuruichao/connection/pool/test/Application.java
+++ src/main/java/com/liuruichao/connection/pool/test/Application.java
@@ -1,7 +1,6 @@
 package com.liuruichao.connection.pool.test;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
